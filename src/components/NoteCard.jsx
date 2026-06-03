467e760da383bb3347390d5c30eaed2f6bcc2e37
amandagm77 <amanda.gmb@outlook.com>
2026-06-03 15:23:19 -0600

    Building NoteCard component

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,4 +1,14 @@
 function NoteCard({ note, deleteNote, startEdit }) {
+  return (
+    <div className="note-card">
+      <p>{note.text}</p>
+
+      <div className="buttons">
+        <button onClick={() => startEdit(note)}>Edit</button>
+        <button onClick={() => deleteNote(note.id)}>Delete</button>
+      </div>
+    </div>
+  );
 }
 
 export default NoteCard;
