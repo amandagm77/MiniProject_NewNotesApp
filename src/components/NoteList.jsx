8a430de96e85292e919ec9f9b99ff8c5822b7f85
amandagm77 <amanda.gmb@outlook.com>
2026-06-03 15:22:19 -0600

    Building NoteList component

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,6 +1,22 @@
 import NoteCard from "./NoteCard";
 
 function NoteList({ notes, deleteNote, startEdit }) {
+  if (notes.length === 0) {
+    return <p>No notes found.</p>;
+  }
+
+  return (
+    <div className="note-list">
+      {notes.map((note) => (
+        <NoteCard
+          key={note.id}
+          note={note}
+          deleteNote={deleteNote}
+          startEdit={startEdit}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default NoteList;
