96902283196939d5a31b1cbbefaaae2e800676ed
amandagm77 <amanda.gmb@outlook.com>
2026-06-03 15:21:12 -0600

    Building NoteForm component

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,6 +1,43 @@
 import { useEffect, useState } from "react";
 
-function NoteForm ({ addNote, editingNote, updateNote }) {
+function NoteForm({ addNote, editingNote, updateNote }) {
+  const [text, setText] = useState("");
+
+  // If editing, populate input
+  useEffect(() => {
+    if (editingNote) {
+      setText(editingNote.text);
+    }
+  }, [editingNote]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!text.trim()) return;
+
+    if (editingNote) {
+      updateNote(text);
+    } else {
+      addNote(text);
+    }
+
+    setText("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Write a note..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+
+      <button type="submit">
+        {editingNote ? "Update Note" : "Add Note"}
+      </button>
+    </form>
+  );
 }
 
 export default NoteForm;
